2b34015d060d1fad54fcbe63a5207574383d55c3
kashvi rajyaguru <78142666+kashvirajyaguru@users.noreply.github.com>
2021-10-20 12:26:15 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,16 @@
+function getData()
+ {
+     firebase.database().ref("/").on('value',
+     function(snapshot)
+ {  document.getElementById("output").innerHTML ="";
+    snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+    Room_names = childKey;
+//Start code
+ <div>
+ 
+ </div>
+//End code
+});});}
+getData();
+
+function rediectToRoomName()
